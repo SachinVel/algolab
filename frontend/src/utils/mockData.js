5eb4edfd624bdc1fecb3161cff96b0f6a9247893
kmahesh <keerthan.mahesh@tabnerglobal.com>
2023-11-19 17:19:20 -0500

    added mockData

--- frontend/src/utils/mockData.js
+++ frontend/src/utils/mockData.js
@@ -1,9 +1,36 @@
 const sampleData = {
-    'registerPostAPI' : {
-        path : '/register',
-        method : 'post',
+    'registerUser' : {
+        path : '/user/register',
+        method : 'POST',
         requestBody : {
-            username : 'john'
+            firstName: 'anurag',
+            lastName: 'kumar',
+            email: 'hey@gmail.com',
+            username : 'kareena',
+            password: 'kapoor',
+            role: 'ADMIN', // ADMIN or STUDENT OR INSTRUCTOR
+            bio: 'whatever',
         }
+    },
+    'updateUser' : {
+        path : '/user/update',
+        method : 'PUT',
+        requestBody : {
+            firstName: 'anurag',
+            lastName: 'kumar',
+            email: 'hey@gmail.com',
+            username : 'kareena',
+            password: 'kapoor',
+            role: 'ADMIN', // ADMIN or STUDENT OR INSTRUCTOR
+            bio: 'whatever',
+        }
+    },
+    'deleteUser' : {
+        path : '/delete/{userId}',
+        method : 'DELETE'
+    },
+    'validateUserName' : {
+        path : '/validate-username?username=anuragKumar',
+        method : 'GET'
     }
 }
