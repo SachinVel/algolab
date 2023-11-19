89c1a663c3310ec6769ada86d09ae2ad17eb5aca
sachinvel <velsachin98@gmail.com>
2023-11-19 17:00:40 -0500

    add login and register page

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+
+import './App.css';
+import { Alert, Box, CircularProgress, Snackbar } from "@mui/material";
+import { BrowserRouter, Routes, Route, Navigate, Router } from "react-router-dom";
+import React, { lazy, Suspense, useEffect, useState } from "react";
+// import AppLayout from './components/AppLayout';
+// import Sidebar from './components/Sidebar';
+
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Logout = lazy(() => import("./pages/Logout"));
+const Home = lazy(() => import("./pages/Home"));
+
+function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    let userToken = localStorage.getItem('token');
+    console.log('userToken : ', userToken);
+    if (userToken != null && userToken != '') {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, []);
+
+
+  return (
+
+    <>
+      <BrowserRouter>
+        <Suspense
+          fallback={
+            <Box className="display-center">
+              <CircularProgress sx={{ margin: "auto" }} />
+            </Box>
+          }>
+          <Box>
+            <Routes>
+            <Route path="/" exact element={loggedIn ? <Navigate to='/home' state={{ isLoggedIn: true }} /> : <Navigate to='/login' state={{ showLoginNecessary: true }} />} />
+              <Route path="/login" exact element={loggedIn ? <Navigate to='/home' state={{ isLoggedIn: true }} /> : <Login setLoggedIn={setLoggedIn} />} />
+              <Route path="/register" element={<Register setLoggedIn={setLoggedIn}/>} />
+              <Route path="/logout" element={<Logout setLoggedIn={setLoggedIn}/>} />
+              <Route path="/home" element={loggedIn ?  <Home /> : <Navigate to='/login' state={{ showLoginNecessary: true }} />} />
+            </Routes>
+          </Box>
+        </Suspense>
+      </BrowserRouter>
+    </>
+
+
+
+
+
+  );
+}
+
+export default App;
