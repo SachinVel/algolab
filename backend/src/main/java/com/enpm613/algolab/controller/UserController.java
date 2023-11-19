0739fd4848d11924bb7b0a0e5688c2792ca0480a
kmahesh <keerthan.mahesh@tabnerglobal.com>
2023-11-19 16:43:47 -0500

    user functionalities

--- backend/src/main/java/com/enpm613/algolab/controller/UserController.java
+++ backend/src/main/java/com/enpm613/algolab/controller/UserController.java
@@ -1,7 +1,33 @@
 package com.enpm613.algolab.controller;
 
-import org.springframework.stereotype.Controller;
+import com.enpm613.algolab.entity.user.User;
+import com.enpm613.algolab.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/user")
+@AllArgsConstructor
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> registerUser(@RequestBody User user) {
+        try {
+            return ResponseEntity.ok(userService.registerUser(user));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/validate-username")
+    public ResponseEntity<Boolean> validateUsername(@RequestBody String username) {
+        return ResponseEntity.ok(userService.isValidUsername(username));
+    }
 }
--- backend/src/main/java/com/enpm613/algolab/service/UserService.java
+++ backend/src/main/java/com/enpm613/algolab/service/UserService.java
@@ -1,7 +1,33 @@
 package com.enpm613.algolab.service;
 
+import com.enpm613.algolab.entity.user.User;
+import com.enpm613.algolab.repository.UserRepository;
+import com.enpm613.algolab.util.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    Validator validator;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User registerUser(User user) {
+        if (!validator.validateUsername(user.getUsername())) {
+            throw new RuntimeException("Username already exists.");
+        }
+        if (!validator.validateEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists.");
+        }
+        if (!validator.validatePassword(user.getPassword())) {
+            throw new RuntimeException("Password is not strong enough.");
+        }
+        return userRepository.save(user);
+    }
+
+    public boolean isValidUsername(String username) {
+        return validator.validateUsername(username);
+    }
 }
