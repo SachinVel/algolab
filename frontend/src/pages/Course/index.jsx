b1aab23d2ead8073afee95a61b0721488caef7fe
sachinvel <velsachin98@gmail.com>
2023-11-27 23:42:31 -0500

    fix authentication issue

--- frontend/src/pages/Course/index.jsx
+++ frontend/src/pages/Course/index.jsx
@@ -1,22 +1,33 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert, Box, Button, CircularProgress, Snackbar, Stack, Typography } from "@mui/material";
-import { BrowserRouter, Routes, Route, Navigate, Router } from "react-router-dom";
-import { lazy, Suspense, useEffect, useState } from "react";
 import styles from './course.module.css';
 import Header from '../../components/Header';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Course() {
 
-    const greeting = 'Hello Function Component!';
+    const [courses, setCourses] = useState([]);
+    const [role, setRole] = useState('');
+
+    useEffect(() => {
+
+        let role = window.localStorage.getItem('role');
+        setRole(role);
+    }, []);
+
 
 
 
     return (
         <>
             <Header />
-            
             <Stack className={styles.bodyContainer}>
+                <Stack flexDirection="row" justifyContent="right" sx={{ padding: '20px' }}>
+                    <Button variant='contained'>
+                        <AddIcon /> Course
+                    </Button>
+                </Stack>
 
             </Stack>
         </>
