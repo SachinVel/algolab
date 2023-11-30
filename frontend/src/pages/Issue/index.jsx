dc2c032e5f3d7b59b7db2df957b1c32ea969a4b0
kmahesh <keerthan.mahesh@tabnerglobal.com>
2023-11-30 18:46:29 -0500

    issue accordion change

--- frontend/src/pages/Issue/index.jsx
+++ frontend/src/pages/Issue/index.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Button, TextField, Link, Box, Typography, Stack, InputLabel, Select, MenuItem, Grid, Paper, List, ListItem, ListItemText } from '@mui/material';
+import {
+    Button,
+    Box,
+    Typography,
+    AccordionSummary, AccordionDetails, Accordion
+} from '@mui/material';
 
 import MuiAlert from '@mui/material/Alert';
-import { useNavigate } from 'react-router-dom';
 import styles from './issue.module.css';
 import Snackbar from '@mui/material/Snackbar';
 import backendCall from '../../utils/network';
-
 import Header from '../../components/Header';
+import DeleteIcon from "@mui/icons-material/Delete";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-
 export default function Issue() {
-
     const [token, setToken] = useState('');
     const [role, setRole] = useState('');
 
     const [issues, setIssues] = useState([]);
-    const [curIssue, setCurIssue] = useState(null);
 
     const [message, setMessage] = useState('');
     const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
     const [snackType, setSnackType] = useState(false);
 
-
-    const hanldeSnackbarClose = () => {
+    const handleSnackbarClose = () => {
         setIsSnackbarOpen(false);
     }
 
@@ -49,7 +50,6 @@ export default function Issue() {
     }, []);
 
     const getIssues = async (token) => {
-
         let config = {
             headers: {
                 'Authorization': `Bearer ${token}`
@@ -69,31 +69,6 @@ export default function Issue() {
             }
         });
     }
-
-    const getIssueById = async (token, issueId) => {
-
-        let config = {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        };
-
-        await backendCall.get('/api/v1/issues/' + issueId, config).then((res) => {
-            setCurIssue(res.data);
-        }).catch((err) => {
-            console.log('err : ', err);
-            setMessage(err.response.data.error);
-            setSnackType('error');
-            setIsSnackbarOpen(true);
-        });
-
-
-
-    }
-
-    const handleIssueClick = (issue) => {
-        getIssueById(token, issue.id);
-    };
 
     const deleteIssue = (issueId) => {
         let config = {
@@ -115,61 +90,48 @@ export default function Issue() {
         <>
             <Header />
             <Box className={styles.bodyContainer}>
-                <Grid container spacing={2}>
-                    {/* Left Pane (30%) */}
-                    <Grid item xs={3}>
-                        <Paper elevation={3} style={{ height: '100%', overflow: 'auto' }}>
-                            <List>
-                                {issues.map((issue) => (
-                                    <ListItem key={issue.id} onClick={() => handleIssueClick(issue)}>
-                                        <ListItemText primary={issue.name} />
-                                        <Button onClick={()=>{deleteIssue(issue.id)}}>Delete</Button>
-                                    </ListItem>
-                                ))}
-                            </List>
-                        </Paper>
-                    </Grid>
-
-                    {/* Right Pane (70%) */}
-                    <Grid item xs={9}>
-                        <Paper elevation={3} style={{ padding: '20px', height: '100%' }}>
-                            {curIssue ? (
-                                <div>
-                                    <Typography variant="h5">User:{curIssue.user.username}</Typography>
-                                    <Typography variant="h5">Title:{curIssue.name}</Typography>
-                                    <Typography variant="body1">Description:{curIssue.description}</Typography>
-                                    <Typography variant="body1">Severity:{curIssue.severity}</Typography>
-
-                                </div>
-                            ) : (
-                                <Typography variant="body1">Select an issue to view details.</Typography>
-                            )}
-                        </Paper>
-                    </Grid>
-                </Grid>
+                {issues.map((issue) => (
+                    <Accordion key={issue.id} style={{paddingBottom: '16px'}}>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography style={{fontWeight: 'bold'}}>{issue.name}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography variant="h5">User: {issue.user.username}</Typography>
+                            <Typography variant="h5">Title: {issue.name}</Typography>
+                            <Typography variant="body1">Description: {issue.description}</Typography>
+                            <Typography variant="body1">Severity: {issue.severity}</Typography>
+                        </AccordionDetails>
+                        {(role === 'ADMIN' || role === 'INSTRUCTOR') && (
+                            <Button
+                                onClick={() => {deleteIssue(issue.id)}}
+                                variant="outlined"
+                                color="error"
+                                startIcon={<DeleteIcon />}
+                            >
+                                Delete
+                            </Button>
+                        )}
+                    </Accordion>
+                ))}
             </Box>
-
             <Snackbar
                 open={isSnackbarOpen}
                 autoHideDuration={4000}
-                onClose={hanldeSnackbarClose}
+                onClose={handleSnackbarClose}
                 disableWindowBlurListener={true}
             >
                 <Box>
                     {
-                        message != '' &&
+                        message !== '' &&
                         <>
-                            <Alert onClose={hanldeSnackbarClose} severity={snackType}>
+                            <Alert onClose={handleSnackbarClose} severity={snackType}>
                                 {message}
                             </Alert>
                             <br></br>
                         </>
                     }
                 </Box>
-
             </Snackbar>
         </>
-
-
     );
 }
